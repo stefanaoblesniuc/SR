26f8598f99c3d75b5a9cc77535f9f8dbf879ace5
zeche <zecheruandreea@yahoo.com>
2024-12-06 22:18:05 +0200

    connect Recombee, read csv file, add .gitignore file

--- MovieApp/Program.cs
+++ MovieApp/Program.cs
@@ -1,6 +1,24 @@
+using MovieApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Configurarea bazei Recombee
+string recombeeDatabase = "proiect-recom-filme-prod";
+string recombeeToken = "JQBPtARfOrmt46i19cedXCkxGPuC50H0LX4e1RCdFIT9SwjMqt3ZIhj4urETDbP3";
+
+// Inițializare RecombeeService
+var recombeeService = new RecombeeService(recombeeDatabase, recombeeToken);
+
+// Configurarea bazei de date
+// recombeeService.ConfigureDatabase();
+
+// Încărcarea datelor din CSV
+string csvFilePath = "NetflixOriginals.csv";
+recombeeService.LoadDataFromCsv(csvFilePath);
+
+
+builder.Services.AddSingleton(recombeeService);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
